7135b131ff37c79931fe3d91d55fec9f7b0d488e
Jhorm4n <jhormanbustamante2309@gmail.com>
2025-08-04 01:08:13 -0500

    Se dan acceso a url de pnd para que no sea bloqueada por cors"

--- src/main/java/com/jhorma/java/springboot/backend/WebConfig.java
+++ src/main/java/com/jhorma/java/springboot/backend/WebConfig.java
@@ -1,4 +1,23 @@
 package com.jhorma.java.springboot.backend;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
 public class WebConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("https://spiffy-semolina-fc17c3.netlify.app")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowedHeaders("*");
+            }
+        };
+    }
 }
